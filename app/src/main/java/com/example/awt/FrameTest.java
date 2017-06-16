d505eaa80a5b42f18fcae661c04e63c29bea3605
longtangxi <876271327@qq.com>
2017-06-16 17:20:35 +0800

    Swing编程

--- app/src/main/java/com/example/awt/FrameTest.java
+++ app/src/main/java/com/example/awt/FrameTest.java
@@ -1,7 +1,5 @@
 package com.example.awt;
 
-import javafx.scene.layout.BorderPane;
-
 /**
  * Created by ty on 2017/6/16.
  */
@@ -10,6 +8,6 @@ public class FrameTest {
 
 
     public static void main(String[] args) {
-        BorderPane roo
+
     }
 }
