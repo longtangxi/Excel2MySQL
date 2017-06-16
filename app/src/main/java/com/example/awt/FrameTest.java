7a1b5e58b499a9b4ed778aff8bb206ce0deaa4ca
longtangxi <876271327@qq.com>
2017-06-16 15:57:34 +0800

    下班

---
+++ app/src/main/java/com/example/awt/FrameTest.java
@@ -0,0 +1,15 @@
+package com.example.awt;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Created by ty on 2017/6/16.
+ */
+
+public class FrameTest {
+
+
+    public static void main(String[] args) {
+        BorderPane roo
+    }
+}
